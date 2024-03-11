a3528209023e7137340b93b974d1faf6bf753971
Asdf123ah <0320-0770@lspu.edu.ph>
2024-03-11 22:20:50 +0800

    Edit splash, about, help, and contact to responsive

--- src/SplashScreen.js
+++ src/SplashScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import gamePictureLogo from './Photos/gamePictureLogo.png';
+import './SplashScreen.css';
 
 function SplashScreen() {
   const navigate = useNavigate();
@@ -9,41 +10,11 @@ function SplashScreen() {
     navigate('/login-form');
   };
 
-  const styleSheet = (
-    <style>
-      {`
-        @keyframes pulse {
-          0% { transform: scale(1); }
-          50% { transform: scale(1.1); }
-          100% { transform: scale(1); }
-        }
-      `}
-    </style>
-  );
-
   return (
-    <div style={splashStyle} onClick={handleStart}>
-      {styleSheet}
-      <img src={gamePictureLogo} alt="Game Logo" style={imageStyle} />
+    <div className='splash-screen' onClick={handleStart}>
+      <img className='image-splash' src={gamePictureLogo} alt="Game Logo" />
     </div>
   );
 }
-
-const splashStyle = {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  justifyContent: 'center',
-  width: '100vw',
-  height: '100vh',
-  backgroundColor: '#84BCC8',
-  cursor: 'pointer',
-};
-
-const imageStyle = {
-  width: '50%',
-  height: 'auto',
-  animation: 'pulse 3s infinite',
-};
 
 export default SplashScreen;
