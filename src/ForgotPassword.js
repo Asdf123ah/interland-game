503aebb33fdb51f451c9bba02f56b9ef0e4537bf
Asdf123ah <0320-0770@lspu.edu.ph>
2024-03-25 15:58:43 +0800

    feat: forgot password not found modal

--- src/ForgotPassword.js
+++ src/ForgotPassword.js
@@ -8,33 +8,40 @@ function ForgotPassword() {
   const [email, setEmail] = useState("");
   const [newPassword, setNewPassword] = useState("");
   const [resetSuccess, setResetSuccess] = useState(false);
-  const [showModalPassChangeSuccess, setShowModalPassChangeSuccess] = useState(false);
-
+  const [showModalPassChangeSuccess, setShowModalPassChangeSuccess] =
+    useState(false);
+  const [emailNotFound, setEmailNotFound] = useState(false); // New state variable
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
+
     try {
-      const response = await axios.post("http://localhost:4000/api/reset-password", { email, newPassword });
+      const response = await axios.post(
+        "http://localhost:4000/api/reset-password",
+        { email, newPassword }
+      );
       const { data } = response;
-      
+
       if (data && data.userId) {
         console.log("User ID:", data.userId);
         setResetSuccess(true);
       } else {
-        console.log("User not found");
+        setEmailNotFound(true); // Set email not found state
       }
     } catch (error) {
       console.error("Error resetting password:", error);
+      setEmailNotFound(true); // Set email not found state in case of error
     }
   };
-  
 
   const handleNewPasswordSubmit = async (e) => {
     e.preventDefault();
-    
+
     try {
-      await axios.post("http://localhost:4000/api/reset-password", { email, newPassword });
+      await axios.post("http://localhost:4000/api/reset-password", {
+        email,
+        newPassword,
+      });
       setResetSuccess(false);
       setShowModalPassChangeSuccess(true);
       setTimeout(() => {
@@ -49,7 +56,6 @@ function ForgotPassword() {
     navigate("/login-form");
   };
 
-
   return (
     <div className="containerForgotPass">
       <button
@@ -61,7 +67,9 @@ function ForgotPassword() {
           <h1>Forgot Password</h1>
           {!resetSuccess ? (
             <>
-              <p>Please enter your email address below to reset your password.</p>
+              <p>
+                Please enter your email address below to reset your password.
+              </p>
               <form onSubmit={handleSubmit}>
                 <input
                   type="email"
@@ -75,7 +83,9 @@ function ForgotPassword() {
             </>
           ) : (
             <>
-              <p>To reset your password, kindly input your new password below.</p>
+              <p>
+                To reset your password, kindly input your new password below.
+              </p>
               <form onSubmit={handleNewPasswordSubmit}>
                 <input
                   type="password"
@@ -92,11 +102,24 @@ function ForgotPassword() {
       </div>
       {showModalPassChangeSuccess && (
         <div className="modal">
-          <div className="modal-content-passchangesuccess ">
-            <span className="close" onClick={() => setShowModalPassChangeSuccess(false)}>
+          <div className="modal-content-passchangesuccess">
+            <span
+              className="close"
+              onClick={() => setShowModalPassChangeSuccess(false)}
+            >
               &times;
             </span>
             <p>Password successfully updated.</p>
+          </div>
+        </div>
+      )}
+      {emailNotFound && (
+        <div className="modal">
+          <div className="modal-content-noemailfound">
+            <span className="close" onClick={() => setEmailNotFound(false)}>
+              &times;
+            </span>
+            <p>Email not found. Try again.</p>
           </div>
         </div>
       )}
